f9f0c584193f3925a50b8284ed29980f3902fb1f
Jubril Siji <oyesiji@gmail.com>
2021-05-03 22:04:24 -0400

    Made changes

--- demo-docker/src/main/java/com/example/demodocker/DemoController.java
+++ demo-docker/src/main/java/com/example/demodocker/DemoController.java
@@ -2,6 +2,8 @@ package com.example.demodocker;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,10 +14,12 @@ public class DemoController
     @RequestMapping("/")
     public List<Customer> findAll()
     {
+    	Map<String, String> env = System.getenv();
         List<Customer> customerList = new ArrayList<Customer>();
         customerList.add(new Customer(1, "frank"));
         customerList.add(new Customer(2, "john"));
         customerList.add(new Customer(3, "jubril"));
+        customerList.add(new Customer(4, env.get("OPENSHIFT_LASTNAME")));
         return customerList;
     }
 }
